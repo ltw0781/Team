18c3b74dad2082f5e3053b9ed76edb2a47256de8
JongsikLEE01 <sononot445@gmail.com>
2024-02-26 14:39:06 +0900

    미션12 코드 설명 추가

--- JavaMission/src/Mission/Mission12.java
+++ JavaMission/src/Mission/Mission12.java
@@ -11,19 +11,21 @@ public class Mission12 {
 		System.out.print("몇게임? ");
 		int game = sc.nextInt();
 		
-		// 번호 생성
 		for (int i = 0; i < game; i++) {
+			// 번호 생성
 			HashSet<Integer> lotto = new HashSet<Integer>();
 			while(lotto.size() < 6) {
 				int num = (int) (Math.random()*45+1);
 				lotto.add(num);
 			}
 			
+			// 번호 arrayList에 저장
 			ArrayList<Integer> arr = new ArrayList<Integer>(lotto);
 			int res = 0;
 			for (int j = 0; j < arr.size(); j++) {
 				res = arr.get(j);
 			}
+			// 정렬 및 출력
 			Collections.sort(arr);
 			System.out.println("["+i+" 게임]"+arr);
 		}
